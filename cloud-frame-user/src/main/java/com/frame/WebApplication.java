0e5f14848d97e890973afdeee847595d5641960d
duanchangqing90@163.com <woai7227440>
2019-02-13 11:25:56 +0800

    去掉无用代码

--- cloud-frame-user/src/main/java/com/frame/WebApplication.java
+++ cloud-frame-user/src/main/java/com/frame/WebApplication.java
@@ -1,11 +1,9 @@
 package com.frame;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
-@EnableCaching
 public class WebApplication {
 
     public static void main(String[] args) {
