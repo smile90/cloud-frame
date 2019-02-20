e64bf12f1b60268266e4d7cf36da777cba5a0266
duanchangqing90@163.com <woai7227440>
2019-02-20 16:59:17 +0800

    多数据源配置

---
+++ cloud-frame-user/src/main/java/com/frame/datasource/DataSourceTypeEnum.java
@@ -0,0 +1,25 @@
+package com.frame.datasource;
+
+public enum DataSourceTypeEnum {
+
+    PRIMARY("primary", "主数据源"), READ("read", "只读数据源");
+
+    private String name;
+    private String text;
+    private DataSourceTypeEnum(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+}
